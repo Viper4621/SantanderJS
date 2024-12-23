0cfad34d761da7a571e903a455d8fef937532537
Erick <cdzbr46211@gmail.com>
2024-12-23 10:47:31 -0300

    começando aulas

---
+++ aula-13/assincronismo.js
@@ -0,0 +1,13 @@
+const fs = require('fs');
+
+//1* callbacks
+
+//função assincrona (ler arquivo leva um tempo)
+
+fs.readFile('aula-13/arquivo.txt',(erro, conteudoDoArquivo) => {
+    if(erro){
+        console.log("Ocorreu erro ao tentar ler arquivo", erro);
+    } else {
+        console.log(conteudoDoArquivo);
+    }
+})
