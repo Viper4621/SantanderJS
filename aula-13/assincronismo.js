060a20afb3ce4dfac884edcea3c7063c6af3546c
Erick <cdzbr46211@gmail.com>
2024-12-23 10:59:34 -0300

    atualizando

--- aula-13/assincronismo.js
+++ aula-13/assincronismo.js
@@ -8,6 +8,6 @@ fs.readFile('aula-13/arquivo.txt',(erro, conteudoDoArquivo) => {
     if(erro){
         console.log("Ocorreu erro ao tentar ler arquivo", erro);
     } else {
-        console.log(conteudoDoArquivo);
+        console.log(String(conteudoDoArquivo));
     }
 })
